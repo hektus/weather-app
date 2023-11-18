fdb9c8fa604f93c155c3d36b3dc496599c1bb999
Dawid Dzierzega <70227471+hektus@users.noreply.github.com>
2023-11-18 11:40:23 +0100

    display img to suitable weather (rain, snow etc..)

--- main.js
+++ main.js
@@ -16,16 +16,57 @@ const showWeather = () => {
       return response.json();
     })
     .then((json) => {
-      displayWeatherInfo(json.main.temp, json.weather[0].description);
+      displayWeatherInfo(
+        json.main.temp,
+        json.weather[0].main,
+        json.main.humidity,
+        json.wind.speed
+      );
     })
     .catch((err) => console.log(err));
 };
 
-function displayWeatherInfo(temp, desc) {
+function displayWeatherInfo(temp, desc, hum, wind) {
   const temperature = document.querySelector(".temperature");
   const description = document.querySelector(".description");
-  temperature.textContent = temp;
+  const humidity = document.querySelector(".humidity--percent");
+  const windSpeed = document.querySelector(".wind--speed");
+  const weatherImg = document.querySelector(".weather__info img");
+
+  //convert kelvin to celsius and fixed to 1 places after comma
+  //display data
+  temperature.textContent = `${(temp - 273.15).toFixed(1)}°C`;
   description.textContent = desc;
+  humidity.textContent = `${hum}%`;
+  windSpeed.textContent = `${wind}km/h`;
+
+  //display IMG
+  weatherImg.src = `./images/${desc}.png`;
+
+  const atmosphere = [
+    "Mist",
+    "Smoke",
+    "Haze",
+    "Dust",
+    "Fog",
+    "Sand",
+    "Ash",
+    "Squall",
+    "Tornado",
+  ];
+
+  if (atmosphere.includes(desc)) {
+    weatherImg.src = `./images/mist.png`;
+  }
+
+  const rain = ["Rain", "Drizzle"];
+  if (rain.includes(desc)) {
+    weatherImg.src = `./images/rain.png`;
+  }
+
+  // if (desc === "Rain") {
+  //   weatherImg.innerHTML = `<img src="./images/${desc}.png" alt="" />`;
+  // }
 }
 
 searchButton.addEventListener("click", showWeather);
